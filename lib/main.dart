58d15ea78ec4fca2137973bca5cc4537e51d9741
Omar ALSultan <140913566+OmrAl@users.noreply.github.com>
2026-01-27 21:44:18 +0200

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,12 @@
+import 'app/my_app.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_app/presentation/layouts/home_layout.dart';
 
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
-  }
-}
 
 
 
