1ee8975b0d51a34991bf718c518b9004ac21707d
Omar ALSultan <140913566+OmrAl@users.noreply.github.com>
2026-01-27 21:45:42 +0200

    Initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:conditional_builder_null_safety/example/example.dart';
 
-import 'package:todo_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
