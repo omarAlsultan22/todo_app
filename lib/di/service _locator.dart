80c1ded4e3b055edc8a32d84a56a1e79efb26509
Omar ALSultan <140913566+OmrAl@users.noreply.github.com>
2026-08-09 11:10:23 +0300

    Initial commit

--- lib/di/service _locator.dart
+++ lib/di/service _locator.dart
@@ -1,6 +1,6 @@
 import 'core/di_core.dart';
 import 'package:get_it/get_it.dart';
-import 'package:todo_app/di/domains/di_evaluation.dart';
+import 'package:todo_app/di/domains/di_tasks.dart';
 
 
 final sl = GetIt.instance;
